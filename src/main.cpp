667e24628c7accdd571f44b4e3ebf918a4da7ceb
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-06-12 11:53:47 +0200

    rename is-pkg-build to is-app0-root

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 std::string APP_ROOT = "/app0/";
 
 int main(void) {
-    if (!std::filesystem::exists("/app0/.is-pkg-build")) {
+    if (!std::filesystem::exists("/app0/.is-app0-root")) {
         APP_ROOT = "/data/homebrew/";
     }
     App app{};
